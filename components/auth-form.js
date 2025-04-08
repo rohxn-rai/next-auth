14a8da572a56cb29f10a8ee268e3ec7bc22223d7
rohxn-rai <rohan.rai932@outlook.com>
2025-04-08 05:24:07 -0400

    modified auth-form - added a mode parems which decides the login or signup state for the home page

--- components/auth-form.js
+++ components/auth-form.js
@@ -5,7 +5,7 @@ import { useActionState } from "react";
 
 import { signup } from "@/actions/auth-acions";
 
-export default function AuthForm() {
+const AuthForm = ({ mode }) => {
   const [formState, formAction] = useActionState(signup, {});
   return (
     <form id="auth-form" action={formAction}>
@@ -28,11 +28,20 @@ export default function AuthForm() {
         </ul>
       )}
       <p>
-        <button type="submit">Create Account</button>
+        <button type="submit">
+          {mode === "login" ? "Login" : "Create Account"}
+        </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode === "login" && (
+          <Link href="/?mode=signup">Create an account.</Link>
+        )}
+        {mode === "signup" && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
-}
+};
+
+export default AuthForm;
